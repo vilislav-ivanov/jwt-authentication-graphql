7d072967d4ed5c2fc05ba9c68b64259a0a5431b1
vilislav-ivanov <vilislav.ivanov.ivanov@gmail.com>
2021-06-23 17:34:52 +0300

    added and setup  type-graphql

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,39 @@
-import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import { buildSchema } from 'type-graphql';
 
-createConnection().then(async connection => {
+import { Hello } from './modules/Hello';
+import { User } from './entity/User';
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+const main = async () => {
+  const app = express();
+  const schema = await buildSchema({
+    resolvers: [Hello],
+  });
+  const appoloServer = new ApolloServer({ schema });
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+  const connection = await createConnection();
+  console.log('Inserting a new user into the database...');
+  const user = new User();
+  user.firstName = 'Timber';
+  user.lastName = 'Saw';
+  user.age = 25;
+  await connection.manager.save(user);
+  console.log('Saved a new user with id: ' + user.id);
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+  console.log('Loading users from the database...');
+  const users = await connection.manager.find(User);
+  console.log('Loaded users: ', users);
 
-}).catch(error => console.log(error));
+  console.log('Here you can setup and run express/koa/any other framework.');
+
+  appoloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('server running at port 4000');
+  });
+};
+
+main();
