6bccca9ec80610b60ef8a72482a1d5f72d1072c9
vilislav-ivanov <vilislav.ivanov.ivanov@gmail.com>
2021-07-14 20:50:59 +0300

    login

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,9 +1,36 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState } from 'react'
+import { useLoginMutation } from '../generated/graphql';
+import {RouteComponentProps} from 'react-router-dom';
+import { setAccessToken } from '../utils/accessToken';
 
-export function Login(): ReactElement {
+interface Props extends RouteComponentProps {}
+export function Login({ history }: Props): ReactElement {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [login] = useLoginMutation();
+  
   return (
-    <div>
-      Login
-    </div>
+    <form onSubmit={async e => {
+      e.preventDefault();
+      console.log('form submit');
+      const { data } = await login({
+        variables: {
+          email,
+          password
+        }
+      })
+      if (data && data.login.accessToken) {
+        const accessToken = data.login.accessToken
+        setAccessToken(accessToken)
+        history.push('/');
+      }
+    }}>
+      <input type="text" value={email} placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+      <br/>
+      <input type="password" value={password} placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+      <br/>
+      <button type='submit'>Login</button>
+    </form>
   )
 }
---
+++ web/src/utils/accessToken.ts
@@ -0,0 +1,7 @@
+let accessToken = '';
+
+export const setAccessToken = (token: string) => {
+  accessToken = token;
+};
+
+export const getAccessToken = () => accessToken;
